5f511055d9c2a43c6d0b0580104890f917eae66d
chen9527190 <2787097563@qq.com>
2026-01-30 04:02:02 +0800

    feat: AI 生成文件 version 33

--- src/configs/routers.ts
+++ src/configs/routers.ts
@@ -1,17 +1,5 @@
-import HOME from '../pages/home.jsx';
-import PROFILE from '../pages/profile.jsx';
 import ADMIN from '../pages/admin.jsx';
-import APP from '../pages/app.jsx';
 export const routers = [{
-  id: "home",
-  component: HOME
-}, {
-  id: "profile",
-  component: PROFILE
-}, {
   id: "admin",
   component: ADMIN
-}, {
-  id: "app",
-  component: APP
 }]
